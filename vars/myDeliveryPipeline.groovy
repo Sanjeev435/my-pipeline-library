7b8de7e57f5e0021b5866ed224fc039812174f5e
Sanjeev435 <Sanjeev435@users.noreply.github.com>
2018-12-15 01:32:32 +0530

    Update myDeliveryPipeline.groovy

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -1,10 +1,10 @@
 
-def call(body) {
+def call(Map<String, String> ) {
     // evaluate the body block, and collect configuration into the object
     def pipelineParams= [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = pipelineParams
-    body()
+ //   body.resolveStrategy = Closure.DELEGATE_FIRST
+  //  body.delegate = pipelineParams
+  //  body()
 
     pipeline {
         // our complete declarative pipeline can go in here
