b298ac18bb4bdb1d99db3bb8e3e5d08fbcca74de
Shweta217 <38592487+Shweta217@users.noreply.github.com>
2019-01-21 00:05:40 +0530

    Update myDeliveryPipeline.groovy

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -2,7 +2,7 @@
 
 
 
-def call(Map<String, String> body) {
+def call(body) {
 
 	println(body)
 
@@ -10,171 +10,81 @@ def call(Map<String, String> body) {
 
     def config = [:]
 
-  //  body.resolveStrategy = Closure.DELEGATE_FIRST
-  //  body.delegate = config
-config = body;
-   // body()
-println(config)
-   // pipelineParams = body
-
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    //config = body;
+    body()
+	
+        println(config)
 	println 'config.BRANCH value :'
-
 	println (config.BRANCH)
-
-//	println pipelineParams.BRANCH
-
-	println 'Map : '
-
-	println body
-
+	
+	println 'Map : ' 
+	print body
+	
 	println 'config : '
-
-	println config
-
-	
-
-	println 'Start pipeline steps'
+	print config
 
 	
-
-	
-
-        // our complete declarative pipeline can go in here
-
-        pipeline {
-
-		agent { label 'Test_Node11' }
-
-            stages {
-
-			// stage checkout git
-
-                stage('checkout git') {
-
-					steps{ 
-
-					sh "git branch:'master', url:'https://github.com/Sanjeev435/spring-petclinic.git'"
-
-					}
-
-                        
-
-		
-
+println 'Start pipeline steps'
+// our complete declarative pipeline can go in here
+pipeline {
+    agent { label 'Test_Node11' }
+   // agent any
+        stages {
+           // stage checkout git
+            stage('checkout git') {
+		steps{ 
+			sh "git branch:'master', url:'https://github.com/Sanjeev435/spring-petclinic.git'"
+		    }
 		}
-
-				
-
-				//checkout git ends
-
-// stage build
-
-                stage('build') {
-
-					steps{
-
-					script{
-
-					if(isUnix()){
-
+          //checkout git ends
+		    
+          // stage build
+           stage('build') {
+		steps{
+		    script{
+			if(isUnix()){
                             sh 'mvn clean package -Dmaven.test.skip=true'
-
                         }
-
                         else{
-
                             bat('mvn clean install -Dmaven.test.skip=true')
-
-                        }
-
-					}
-
-					
-
-					}
-
-                    
-
-                  }
-
-				  // stage build ends
-
-				  
-
-               // stage Test
-
-                stage ('test') {
-
-				steps{
-
-				script{
-
-								    if('yes' == 'yes'){
-
-						if(isUnix()){
-
-							parallel (
-
-								"unit tests": { sh 'mvn test' },
-
-								"integration tests": { sh 'mvn integration-test' }
-
-							)
-
-							}
-
-						else{
-
-							parallel (
-
-								"unit tests": { bat('mvn test')},
-
-								"integration tests": { bat('mvn integration-test')}
-
-							)
-
-						}
-
-					}
-
+                            }
+		       }
+                   }
+               }
+	   // stage build ends
+		    
+           // stage Test
+           stage ('test') {
+	      steps{
+		   script{
+			if('yes' == 'yes'){
+				if(isUnix()){
+				     parallel (
+					"unit tests": { sh 'mvn test' },
+					"integration tests": { sh 'mvn integration-test' }
+				    )
 				}
-
-
-
+				else{
+				     parallel (
+					"unit tests": { bat('mvn test')},
+					"integration tests": { bat('mvn integration-test')}
+				     )
 				}
-
-
-
+			    }
 			}
-
-			
-
-			 // stage Test Ends
-
-		}
-
-		      
-
-	
-
+		     }
+		 }
+		 // stage Test Ends
+	     }
             post {
-
                 failure {
-
                     mail to: 'mrcool435@gmail.com', subject: 'Pipeline failed', body: "${env.BUILD_URL}"
-
                 }
-
-				success{
-
-					println "SUCCESS"
-
-				}
-
+		success{
+		   println "SUCCESS"
+		}
             }
-
-        }
-
-    
-
+	}
 }
